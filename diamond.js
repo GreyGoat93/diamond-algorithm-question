1d1a8acaa43f728c57b408a59cbc8a27224995b8
GreyGoat93 <ahmet.tahaa93@gmail.com>
2022-04-18 17:46:19 +0300

    Refactored

--- diamond.js
+++ diamond.js
@@ -17,6 +17,31 @@ const printWidestLetter = (letter) => {
     console.log(_string + letter);
 }
 
+const printNonWidestLetter = (i, widestLength, indexOfLetter) => {
+    const degreeOfLetter = indexOfLetter + 1 - i; 
+    const sidesLength = degreeOfLetter - 1;
+    let spacesLength;
+    if(degreeOfLetter === indexOfLetter + 1){
+        spacesLength = 0;
+    } else {
+        spacesLength = widestLength - (degreeOfLetter * 2);
+    }
+    
+    let _string = ""
+    if(spacesLength === 0){
+        _string = addSpacesToString(_string, sidesLength);
+        _string += LETTERS[i];
+        _string = addSpacesToString(_string, sidesLength);
+    } else {
+        _string = addSpacesToString(_string, sidesLength);
+        _string += LETTERS[i];
+        _string = addSpacesToString(_string, spacesLength);
+        _string += LETTERS[i];
+        _string = addSpacesToString(_string, sidesLength);
+    }
+    console.log(_string);
+}
+
 const printDiamond = (letter) => {
     if(letter === "A") {
         console.log("A")
@@ -25,52 +50,10 @@ const printDiamond = (letter) => {
     const indexOfLetter = LETTERS.indexOf(letter);
     const widestLength = (indexOfLetter * 2) + 1;
     for(let i = 0; i < indexOfLetter; i++){
-        const degreeOfLetter = indexOfLetter + 1 - i; 
-        const sidesLength = degreeOfLetter - 1;
-        let spacesLength;
-        if(degreeOfLetter === indexOfLetter + 1){
-            spacesLength = 0;
-        } else {
-            spacesLength = widestLength - (degreeOfLetter * 2);
-        }
-        
-        let _string = ""
-        if(spacesLength === 0){
-            _string = addSpacesToString(_string, sidesLength);
-            _string += LETTERS[i];
-            _string = addSpacesToString(_string, sidesLength);
-        } else {
-            _string = addSpacesToString(_string, sidesLength);
-            _string += LETTERS[i];
-            _string = addSpacesToString(_string, spacesLength);
-            _string += LETTERS[i];
-            _string = addSpacesToString(_string, sidesLength);
-        }
-        console.log(_string);
+        printNonWidestLetter(i, widestLength, indexOfLetter);
     }
     printWidestLetter(letter);
     for(let i = indexOfLetter - 1; i >= 0; i--){
-        const degreeOfLetter = indexOfLetter + 1 - i; 
-        const sidesLength = degreeOfLetter - 1;
-        let spacesLength;
-        if(degreeOfLetter === indexOfLetter + 1){
-            spacesLength = 0;
-        } else {
-            spacesLength = widestLength - (degreeOfLetter * 2);
-        }
-        
-        let _string = ""
-        if(spacesLength === 0){
-            _string = addSpacesToString(_string, sidesLength);
-            _string += LETTERS[i];
-            _string = addSpacesToString(_string, sidesLength);
-        } else {
-            _string = addSpacesToString(_string, sidesLength);
-            _string += LETTERS[i];
-            _string = addSpacesToString(_string, spacesLength);
-            _string += LETTERS[i];
-            _string = addSpacesToString(_string, sidesLength);
-        }
-        console.log(_string);
+        printNonWidestLetter(i, widestLength, indexOfLetter);
     }
 }
